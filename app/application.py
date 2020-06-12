ee3d103a2f0b6f9f28a57d597fa650e18d6c234c
Harman Singh <iamgrewal7@gmail.com>
2020-06-11 23:54:00 -0700

    add particle js, updated ui

--- app/application.py
+++ app/application.py
@@ -3,22 +3,28 @@ from forms import UrlForm
 from lib.url_shortner import UrlShortner
 from settings import db, app
 from models import URL
+import sys
 
 BASE_URL = 'http:localhost:5000'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = UrlForm()
+    short_url = None
+    print(form.url.data, file=sys.stdout)
+    print(form.validate_on_submit(), file=sys.stdout)
     if form.validate_on_submit():
         existing_url = URL.query.filter_by(url=form.url.data).first()
+        print(existing_url, file=sys.stdout)
         if existing_url:
-            return f'{BASE_URL}/{UrlShortner.id_to_url(existing_url.id)}'
-        
-        new_url = URL(url=form.url.data)
-        db.session.add(new_url)
-        db.session.commit()
-        return f'{BASE_URL}/{UrlShortner.id_to_url(new_url.id)}'
-    return render_template('index.html', form=form)
+            short_url = f'{BASE_URL}/{UrlShortner.id_to_url(existing_url.id)}'
+        else:
+            new_url = URL(url=form.url.data)
+            db.session.add(new_url)
+            db.session.commit()
+            short_url = f'{BASE_URL}/{UrlShortner.id_to_url(new_url.id)}'
+    print(short_url, file=sys.stdout)
+    return render_template('index.html', form=form, short_url=short_url)
 
 @app.route('/<path:short_url>')
 def redirect_url(short_url):
